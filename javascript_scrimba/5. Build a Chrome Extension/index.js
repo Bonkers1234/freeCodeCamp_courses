ca7079a02ab91f7cadb938dc268deb629abba28a
Bonkers <boroboro1@interia.pl>
2025-02-08 18:19:41 +0100

    Build an extension progress 10/54

---
+++ javascript_scrimba/5. Build a Chrome Extension/index.js
@@ -0,0 +1,25 @@
+
+
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+
+inputBtn.addEventListener('click', () => {
+  console.log('clicked the button')
+  myLeads.push('www.awesomeleads.com')
+  console.log(myLeads)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
