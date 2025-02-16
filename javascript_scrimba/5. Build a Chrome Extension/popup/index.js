bb87ad6a47dbb358ee0da0af53fee6dfdfc70b8d
Bonkers <boroboro1@interia.pl>
2025-02-16 18:20:43 +0100

    Build an extension progress 52/54

--- javascript_scrimba/5. Build a Chrome Extension/popup/index.js
+++ javascript_scrimba/5. Build a Chrome Extension/popup/index.js
@@ -4,34 +4,54 @@ let myLeads = localStorage.getItem('myLeads') ? JSON.parse(localStorage.getItem(
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 console.log(myLeads)
 
 if(myLeads) {
-  renderLeads()
+  render(myLeads)
 }
+
+function render(leads) {
+  let li = ''
+  for(let i = 0; i < leads.length; i++) {
+    // ulEl.innerHTML += `<li>${leads[i]}</li>`
+    // let li = document.createElement('li')
+    // li.textContent = leads[i]
+    // ulEl.appendChild(li)
+    li += `<li><a target='_blank' href='https://${leads[i]}'>${leads[i]}</a></li>`
+  }
+  
+  ulEl.innerHTML = li
+}
+
+tabBtn.addEventListener('click', () => {
+  browser.tabs.query({ active: true, currentWindow: true })
+    .then(tabs => {
+      myLeads.push(tabs[0].url)
+      localStorage.setItem('myLeads', JSON.stringify(myLeads))
+      render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener('dblclick', () => {
+  myLeads = []
+  localStorage.clear()
+  // ulEl.innerHTML = ''
+  render(myLeads)
+})
 
 inputBtn.addEventListener('click', () => {
   if(inputEl.value) {
     myLeads.push(inputEl.value) 
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
     inputEl.value = ''
-    renderLeads()
+    render(myLeads)
     console.log(JSON.parse(localStorage.getItem('myLeads')))
   }
 })
 
-function renderLeads() {
-  let li = ''
-  for(let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
-    // let li = document.createElement('li')
-    // li.textContent = myLeads[i]
-    // ulEl.appendChild(li)
-    li += `<li><a target='_blank' href='https://${myLeads[i]}'>${myLeads[i]}</a></li>`
-  }
-  
-  ulEl.innerHTML = li
-}
+
 
 
 
